7d52e48159f9f1062f16b5c1f9b19b8e2ec51478
Mario Lazzari <mario.lazzari@gmail.com>
2021-12-10 20:03:30 +0100

    Done

--- scope/scope.js
+++ scope/scope.js
@@ -37,3 +37,35 @@ function myFunc() {
 }
 
 myFunc();
+
+console.log("");
+
+// var does not have block scope
+// let and const have block scope
+var varOne = 1;
+let letTwo = 2;
+const constThree = 3;
+
+console.log("global varOne:", varOne);
+console.log("global letTwo:", letTwo);
+console.log("global constThree:", constThree);
+
+function varScope() {
+  var varOne = 11;
+  let letTwo = 22;
+  const constThree = 33;
+
+  {
+    var varOne = 111; // var is function scoped
+    const constThree = 333; // const is block scoped
+    console.log("block varOne:", varOne);
+    console.log("block letTwo:", letTwo);
+    console.log("block constThree:", constThree);
+  }
+
+  console.log("fx varOne:", varOne);
+  console.log("fx letTwo:", letTwo);
+  console.log("fx constThree:", constThree);
+}
+
+varScope();
