9b65d74fee544c0331b5f9b8d00cf845f12b9ae4
Mario Lazzari <mario.lazzari@gmail.com>
2022-02-07 20:40:58 +0100

    Added shallow

--- shallow-deep/main.js
+++ shallow-deep/main.js
@@ -26,3 +26,51 @@ console.log(myName);
 yArray[0] = 9;
 console.log(yArray);
 console.log(xArray);
+
+// const doesn't make array immutable
+
+// shallow copy vs deep copy (clones)
+
+// shallow copy with spread operator
+let zArray = [...yArray, 10];
+console.log("zArray", zArray);
+console.log("yArray", yArray);
+
+console.log("xArray === yArray", xArray === yArray);
+console.log("zArray === yArray", zArray === yArray);
+
+// shallow copy with object assing
+const tArray = Object.assign([], zArray);
+console.log("tArray", tArray);
+console.log("tArray === zArray", tArray === zArray);
+tArray.push(11);
+console.log("tArray", tArray);
+console.log("zArray", zArray);
+
+// but nested objects...
+yArray.push([8, 9, 10]);
+const vArray = [...yArray];
+console.log("vArray", vArray);
+console.log("yArray", yArray);
+vArray[4].push(5);
+console.log("vArray", vArray);
+console.log("yArray", yArray);
+
+// nested structures share references
+
+// freeze object
+const scoreObj = {
+  first: "44",
+  second: "12",
+  third: {
+    a: 1,
+    b: 2,
+  },
+};
+
+// shallow freeze
+Object.freeze(scoreObj);
+scoreObj.third.a = 9;
+console.log(scoreObj); // mutated!
+
+// Deep copy
