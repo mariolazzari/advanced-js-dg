99e67619c2a1d57ad3aa2cb0c5863f2f890ece90
Mario Lazzari <mario.lazzari@gmail.com>
2022-02-12 18:59:20 +0100

    Added pipe

--- pipe-compose/main.js
+++ pipe-compose/main.js
@@ -3,5 +3,23 @@ const add2 = x => x + 2;
 const subtract = x => x - 1;
 const multiplyBy5 = x => x * 5;
 
-const res = multiplyBy5(subtract(add2(4)));
+let res = multiplyBy5(subtract(add2(4)));
+console.log(res);
+
+// right to left
+const compose =
+  (...fns) =>
+  val =>
+    fns.reduceRight((acc, fn) => fn(acc), val);
+
+res = compose(multiplyBy5, subtract, add2)(4);
+console.log(res);
+
+// left to right
+const pipe =
+  (...fns) =>
+  val =>
+    fns.reduce((acc, fn) => fn(acc), val);
+
+res = pipe(add2, subtract, multiplyBy5)(4);
 console.log(res);
